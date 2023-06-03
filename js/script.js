b0a1714f4f0b8f2ab2bb8c03b2a69e0a0faf66cf
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-06-03 20:09:39 +0200

    added alert and navbar

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 console.log('JS OK');
+
+const alert = document.getElementById('alert');
+const closeAlert = document.getElementById('close-alert');
+
+closeAlert.addEventListener('click', function(){
+    alert.classList.add('d-none');
+})
