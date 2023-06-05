84b7a46a7c9904b20a11a4ee6995a4b8b19a2c0e
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-06-05 22:13:34 +0200

    added not working camp add

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 console.log('JS OK');
 
+
+
 // close alert message
 const alert = document.getElementById('alert');
 const closeAlert = document.getElementById('close-alert');
@@ -8,8 +10,14 @@ const inputSearch = document.getElementById('search');
 const searchButton = document.getElementById('searchButton');
 const cardTitiles = document.querySelectorAll('main .card h5');
 const mainCards = document.querySelectorAll('main .col-12');
-console.log(mainCards);
-
+// Add Your own Camp
+const AddCampButton = document.getElementById('submit-camp');
+const AddCampTarget = document.getElementById('camps-row');
+//Add Your own Camps input values
+const campName = document.getElementById('camp-name');
+const campPrice = document.getElementById('camp-price');
+const campImageLink = document.getElementById('camp-image');
+const campDesc = document.getElementById('camp-desc');
 
 // close alert message
 closeAlert.addEventListener('click', function(){
@@ -27,3 +35,23 @@ searchButton.addEventListener('click', function(){
         }
     }
 })
+
+// Add Your own Camp
+
+AddCampButton.addEventListener('click', function(){
+    const userCampName = campName.value;
+    const userCampPrice = campPrice.value;
+    const userCampImageLink = campImageLink.value;
+    const userCampDesc = campDesc.value;
+
+    AddCampTarget.innerHTML = `
+    <div class="col-12 col-md-6 col-lg-4 pb-4">
+        <div class="card p-4">
+            <img class="rounded mb-4" src="${userCampImageLink}">
+            <h5>${userCampName}</h5>
+            <p>${userCampDesc}</p>
+            <div>${userCampPrice}</div>
+        </div>
+    </div>
+    `;
+});
